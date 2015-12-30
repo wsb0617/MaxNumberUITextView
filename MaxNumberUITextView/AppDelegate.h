bc8b978ec04fd1eaee038ce0f203d2ccfe5aa2ed
wsb0617 <747454452@qq.com>
2015-12-30 15:03:06 +0800

    Initial Commit

---
+++ MaxNumberUITextView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MaxNumberUITextView
+//
+//  Created by wsb0617 on 15/12/30.
+//  Copyright © 2015年 wsb0617. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MaxNumberUITextView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MaxNumberUITextView
+//
+//  Created by wsb0617 on 15/12/30.
+//  Copyright © 2015年 wsb0617. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
